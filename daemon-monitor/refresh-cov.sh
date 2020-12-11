fbb67ee78f6de515f18d815cf003d26cc0949122
qian <qifeng@syr.edu>
2020-12-11 15:45:12 -0800

    new

---
+++ daemon-monitor/refresh-cov.sh
@@ -0,0 +1,3 @@
+cp ../cb-multios-cov/build/challenges/Recipe_Database/Recipe_Database ./Recipe_Database.cov
+# cp ../cb-multios-aif/build/include/libcgc.so lib-aif/
+# cp ../cb-multios-aif/build/include/tiny-AES128-C/libtiny-AES128-C.so lib-aif/
